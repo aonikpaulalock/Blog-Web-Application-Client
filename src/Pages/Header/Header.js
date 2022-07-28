07e28923918b9f0b8eabb22f13b62a5fc7ccf1f3
aonikpaulalock <alockpaul9845@gmail.com>
2022-07-28 20:23:31 +0600

    aded-modal

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom"
 import Logo from "../../Asset/Logo/Logo.png"
 import "../../Styles/Header.css"
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 const Header = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <nav class="navbar navbar-expand-lg navber-bg">
+    <nav class="navbar navbar-expand-lg navber-bg sticky-top">
       <div class="container">
         <Link class="navbar-brand" to="/">
           <img src={Logo} alt="" />
@@ -13,15 +18,29 @@ const Header = () => {
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent"> <form class="mt-md-0 mt-5 mx-lg-auto form-width ">
-          <input class="form-control rounded-pill" type="search" placeholder="Search for your favorite groups in ATG" aria-label="Search" />
+          <input class="form-control rounded-pill input-field" type="search" placeholder="Search for your favorite groups in ATG" aria-label="Search" />
         </form>
-          <ul class="navbar-nav mb-2 mb-lg-0">
+          <ul class="navbar-nav mb-2 mb-lg-0" onClick={handleShow}>
             <span class="dropdown account-text">
-              Create Account . 
+              Create Account .
               <Link to="/"> It's Free</Link>
-              <span  className="dropdown-toggle ms-2"></span>
+              <span className="dropdown-toggle"></span>
             </span>
           </ul>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleClose}>
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
       </div>
     </nav>
--- src/Pages/Posts/Posts.js
+++ src/Pages/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from "react-router-dom"
+import "../../Styles/PostsStyles/Posts.css"
 const Posts = () => {
   return (
     <div className="container">
@@ -8,7 +9,31 @@ const Posts = () => {
           <Outlet />
         </div>
         <div className="col-md-4">
-
+          <div className="input-location">
+            <input type="email" className="from-control location-input" placeholder="Noida , India" />
+            <span className="location-position">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+              </svg>
+            </span>
+            <span className="edit-position">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+              </svg>
+            </span>
+          </div>
+          <div className="exclematory-position">
+            <p className="exclematory-text">
+              Your location will help us serve better<br></br> and extend a personalised experience.
+            </p>
+            <span className="position-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-circle me-3" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+              </svg>
+            </span>
+          </div>
         </div>
       </div>
     </div>
