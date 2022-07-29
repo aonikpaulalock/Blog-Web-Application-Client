48de5a354a56f4326aa550513e707475b2d7bc4d
aonikpaulalock <alockpaul9845@gmail.com>
2022-07-29 15:36:58 +0600

    filter not solve

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -17,26 +17,26 @@ const Header = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return (
-    <nav class="navbar navbar-expand-lg navber-bg sticky-top container-fluid">
-      <div class="container">
-        <Link class="navbar-brand" to="/">
+    <nav className="navbar navbar-expand-lg navber-bg sticky-top container-fluid">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
           <img src={Logo} alt="" className="img-fluid" />
         </Link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent"> <form class="mt-md-0 mt-5 mx-lg-auto form-width ps-sm-0 ps-3">
-          <input class="form-control rounded-pill input-field" type="search" placeholder="Search for your favorite groups in ATG" aria-label="Search" />
+        <div className="collapse navbar-collapse" id="navbarSupportedContent"> <form className="mt-md-0 mt-5 mx-lg-auto form-width ps-sm-0 ps-3">
+          <input className="form-control rounded-pill input-field" type="search" placeholder="Search for your favorite groups in ATG" aria-label="Search" />
           <span className="search-position">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
             </svg>
           </span>
         </form>
           {
             !user ?
-              <ul class="navbar-nav mb-2 mb-lg-0 ps-sm-0 ps-4" onClick={handleShow}>
-                <span class="dropdown account-text">
+              <ul className="navbar-nav mb-2 mb-lg-0 ps-sm-0 ps-4" onClick={handleShow}>
+                <span className="dropdown account-text">
                   Create Account .
                   <span className="its-free"> It's Free</span>
                   <span className="dropdown-toggle"></span>
@@ -57,9 +57,9 @@ const Header = () => {
                   }
 
                   <button onClick={() => signOut(auth)} className="signout">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" fill="#D9D9DB" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                      <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
-                      <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" fill="#D9D9DB" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                      <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+                      <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
                     </svg>
                   </button>
                 </div>
--- src/Pages/Posts/AllPosts.js
+++ src/Pages/Posts/AllPosts.js
@@ -8,13 +8,13 @@ const AllPosts = () => {
       <div className="col">
         {
           posts.map(post =>
-            <div class="card mb-3" key={post.id}>
-              <img class="card-img-top img-fluid" src={post.img} alt="" />
-              <div class="card-body">
-                <h5 class="card-title-name">{post.name}</h5>
+            <div className="card mb-3" key={post.id}>
+              <img className="card-img-top img-fluid" src={post.img} alt="" />
+              <div className="card-body">
+                <h5 className="card-title-name">{post.name}</h5>
                 <div className="d-flex justify-content-between align-items-center">
-                  <h1 class="main-title">{post.mainTitle}</h1>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#000000" class="bi bi-three-dots" viewBox="0 0 16 16">
+                  <h1 className="main-title">{post.mainTitle}</h1>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#000000" className="bi bi-three-dots" viewBox="0 0 16 16">
                     <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
                   </svg>
                 </div>
@@ -34,7 +34,7 @@ const AllPosts = () => {
                     {post.location2}
                   </h5>
                 </div>
-                <h6 class="card-sub-title">{post.subTitle}</h6>
+                <h6 className="card-sub-title">{post.subTitle}</h6>
                 <div className="d-flex justify-content-between align-items-center">
                   <div className="d-flex align-items-center">
                     <img src={post.userImg} alt="" />
@@ -44,7 +44,7 @@ const AllPosts = () => {
                     <div className="d-flex align-items-center">
                       <h6>
                         <span className="me-2">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16">
                             <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
                             <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
                           </svg>
@@ -52,7 +52,7 @@ const AllPosts = () => {
                         {post.views}
                       </h6>
                       <h6 className="svg-gap">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-share" viewBox="0 0 16 16">
                           <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
                         </svg>
                       </h6>
